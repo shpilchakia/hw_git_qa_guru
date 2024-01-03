6199f6cc54f0566171798759382d7fd85a73482d
Igor Shpilchak <shpilchak.igor@yandex.ru>
2024-01-04 01:51:28 +0300

    change message

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Не хочу приветствоватьь мир");
+            System.out.println("Не хочу дальше писать");
         }
     }
 }
